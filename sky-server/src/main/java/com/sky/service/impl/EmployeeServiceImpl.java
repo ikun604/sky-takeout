7a2763efbc5ea19f0cb43d9f595b44aa74dfb4ac
ikun604 <2056864142@qq.com>
2024-01-24 21:06:32 +0800

    improve login functionality

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -39,6 +39,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
+        //md5加密
         password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
